9e73d58fb678316ca2ea7cf9f82c483634a63d9a
PEIGNE-Hugo <peigne.hugo85@gmail.com>
2016-02-02 11:59:28 +0100

    modif

---
+++ mod_gci/index.php
@@ -0,0 +1,48 @@
+<?php
+
+function forum_route() {
+    include(ROOT.'AdminLTE/forum.php');
+}
+function timeline(){
+    ?>
+    <ul class="timeline">
+
+    <!-- timeline time label -->
+    <li class="time-label">
+        <span class="bg-red">
+            10 Feb. 2014
+        </span>
+    </li>
+    <!-- /.timeline-label -->
+
+    <!-- timeline item -->
+    <li>
+        <!-- timeline icon -->
+        <i class="fa fa-envelope bg-blue"></i>
+        <div class="timeline-item">
+            <span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+
+            <h3 class="timeline-header"><a href="#">Support Team</a> ...</h3>
+
+            <div class="timeline-body">
+                ...
+                Content goes here
+            </div>
+
+            <div class="timeline-footer">
+                <a class="btn btn-primary btn-xs">...</a>
+            </div>
+        </div>
+    </li>
+    <!-- END timeline item -->
+
+    ...
+
+</ul>
+    
+
+
+
+    <?php
+    echo 'tamer';
+}
