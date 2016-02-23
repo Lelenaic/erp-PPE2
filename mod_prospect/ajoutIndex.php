ceae28b0e94975fcd93a83b2324c13103484dac2
clementdroillard <droillard.clement@neuf.fr>
2016-02-23 14:23:45 +0100

    Update ajoutIndex.php

--- mod_prospect/ajoutIndex.php
+++ mod_prospect/ajoutIndex.php
@@ -1,7 +1,7 @@
-<?php
 
-// Fichier d'arrivé par défaut pour s'identifier d'authentification
-function index_route(){
+<?php
+function index_route()
+{
     $form = new FormBootstrap('Prospect');
     $form->addHidden('route', 'prospect_ajoutIndex_valid');
     $form->addText('nom',array(), 'Nom');
@@ -15,6 +15,16 @@ function index_route(){
      include(ROOT.'AdminLTE/form.php');
 }
 function valid_route(){
+    $nom=$_POST['nom'];
+    $prenom=$_POST['prenom'];
+    $adresse=$_POST['adresse'];
+    $codePostal=$_POST['codePostal'];
+    $ville=$_POST['ville'];
+    $mail=$_POST['mail'];
+    $numTel=$_POST['numTel'];
+    $query='INSERT INTO prospect (nom, prenom, adresse, codePostal, ville, mail, numtelephone)'
+            . "VALUES ('".$nom."', '".$prenom."', '".$adresse."', '".$codePostal."', '".$ville."', '".$mail."', '".$numTel."')";
+    Connexion::exec($query);
     include(ROOT.'AdminLTE/alerte.php');
-     
+    
 }
