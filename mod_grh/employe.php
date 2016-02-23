bb75b8fcd505ce2270f766cea3faa3b7d6c32a4b
etienne <etienne@gmail.com>
2016-02-23 14:16:34 +0100

    merge

--- mod_grh/employe.php
+++ mod_grh/employe.php
@@ -1,8 +1,5 @@
 <?php
 
-
-
-
 function formAjouter_route(){
     $form=new FormBootstrap();
     $form->addHidden('route','grh_employe_ajouter');
@@ -30,44 +27,3 @@ function liste_route(){
     include(ROOT.'AdminLTE/grh/employe/liste.php');
 }
 
-//menuUtilisateur
-function menuUtilisateur($menuParent_id = null) {
-    $utilisateur = $_SESSION['utilisateur'];
-    $query = 'select * '
-            . 'from menu m, utilisateurtype_menu um '
-            . 'where um.utilisateurtype_id=' . $utilisateur['utilisateurtype_id']
-            . ' and m.id=um.menu_id';
-    if (is_null($menuParent_id)) {
-        $query.= ' and m.menuparent_id is null';
-    } else {
-        $query.= ' and m.menuparent_id=' . $menuParent_id;
-    }
-    $query.=' order by um.ordre';
-
-    $menus = Connexion::table($query);
-
-    $html = '';
-    foreach ($menus as $menu) {
-        if($menu['route']==''){
-            $url='#';
-        }else{
-            $url='.?route='.$menu['route'];
-        }
-        $enfants=  menuUtilisateur($menu['id']);
-        if($enfants!=''){
-            $class=' class="treeview"';
-        }else{
-            $class='';
-        }
-        $html.='<li'.$class.'>
-                <a href="'.$url.'">
-                <i class="fa '.$menu['icon'].'"></i> <span>'.$menu['label'].'</span>
-                </a>';
-        if($enfants!=''){
-            $html.='<ul class="treeview-menu">'.$enfants.'</ul>';
-        }
-        $html.='</li>';
-
-    }
-    return $html;
-}
