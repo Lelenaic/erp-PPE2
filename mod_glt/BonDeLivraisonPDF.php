14539b6cb4da4ad48e825603977f7decf24a80b7
DavignonCorentin <you@example.com>
2016-03-15 11:07:51 +0100

    PDF BonLivraison

--- mod_glt/BonDeLivraisonPDF.php
+++ mod_glt/BonDeLivraisonPDF.php
@@ -1,5 +1,7 @@
 <?php
 require('fpdf181/fpdf.php');
+include ('fonctionsBonDeLivraison.php');
+
 
 class PDF extends FPDF
 {
@@ -19,20 +21,26 @@ class PDF extends FPDF
 	{
 		// En-tête
 		//foreach($header as $col)
-			$this->Cell(126,7,$header[0],1);
-			$this->Cell(43,7,$header[1],1);
-			$this->Cell(21,7,$header[2],1);
+			$this->Cell(83,9,$header[0],1);
+			$this->Cell(35,9,$header[1],1);
+			$this->Cell(26,9,$header[2],1);
+			$this->Cell(20,9,$header[3],1);
+			$this->Cell(26,9,$header[4],1);
 			$this->Ln();
 		// Données
+		$this->SetFont('Arial','',10);
 		foreach($data as $row)
 		{
 			//foreach($row as $col)
-				$this->Cell(126,8,$row['libelle'],1);
-				$this->Cell(43,8,$row['reference'],1);
-				$this->Cell(21,8,$row['quantite'],1);
+				$this->Cell(83,8,$row['libelle'],1);
+				$this->Cell(35,8,$row['reference'],1);
+				$this->Cell(26,8,$row['montant'],1);
+				$this->Cell(20,8,$row['quantite'],1);
+				$this->Cell(26,8,$row['montant']*$row['quantite'],1);
 			$this->Ln();
 		}
 	}
+	
 	
 }
 
@@ -45,7 +53,7 @@ $pdf->SetFont('Arial','',14);
 $pdf->Cell(0,10,'BON DE LIVRAISON','TBRL',1,'C');
 $pdf->Cell(0,5,'','',1,'R');
 
-$infoEntrepriseFournisseur=bonDeLivraisonEntrepriseFournisseur(1);
+$infoEntrepriseFournisseur=bonDeLivraisonEntrepriseFournisseur(2);
 //var_dump($infoEntrepriseFournisseur);
 
 $pdf->Cell(0,10,$infoEntrepriseFournisseur[0]['nom'],'',1);			//"Entreprise fournisseur" deviendra '$infoEntrepriseFournisseur[0][0]'
@@ -53,7 +61,7 @@ $pdf->Cell(0,0,$infoEntrepriseFournisseur[0]['adresse'],'',1);							//"Adresse"
 $pdf->Cell(0,10,$infoEntrepriseFournisseur[0]['ville'].' '.$infoEntrepriseFournisseur[0]['codePostal'],'',1);			//"Adresse" deviendra '$infoEntrepriseFournisseur[0][3].' '.$infoEntrepriseFournisseur[0][1]'
 $pdf->Cell(0,0,$infoEntrepriseFournisseur[0]['tel'],'',1);					//"Adresse" deviendra '$infoEntrepriseFournisseur[0][4]'
 
-$infoEntrepriseCliente=bonDeLivraisonEntrepriseCliente(1);
+$infoEntrepriseCliente=bonDeLivraisonEntrepriseCliente(2);
 $pdf->Cell(0,10,'','',1,'R');
 
 $pdf->Cell(0,10,$infoEntrepriseCliente[0]['libelle'],0,1,'R');			//"Entreprise fournisseur" deviendra '$infoEntrepriseCliente[0][0]'
@@ -62,10 +70,11 @@ $pdf->Cell(0,10,$infoEntrepriseCliente[0]['ville'].' '.$infoEntrepriseCliente[0]
 $pdf->Cell(0,0,$infoEntrepriseCliente[0]['numero'],'',1,'R');				//"Adresse" deviendra '$infoEntrepriseCliente[0][4]'
 $pdf->Cell(0,15,'','',1,'R');
 
-$pdf->Cell(0,10,'PRODUITS LIVRES','TB',1,'C');
+$pdf->Cell(0,10,'PRODUITS A LIVRER','TB',1,'C');
 $pdf->Cell(0,5,'','',1,'R');
-$header = array('Article', 'Reference Produit', 'Quantite',);
+$header = array('Article', 'Reference Produit','Prix Uni. HT','Quantite','Prix Total HT');
 $data = bonDeLivraisonProduit(1);
+$pdf->SetFont('Arial','',11);
 $pdf->BasicTable($header,$data);
 
 $pdf->Output();
@@ -75,32 +84,4 @@ $pdf->Output();
 
 
 
-
-function bonDeLivraisonEntrepriseFournisseur($idEntreprise)
-{
-	$requeteEntrepriseFournisseur="SELECT nom,codePostal,adresse,ville,tel FROM fournisseur WHERE id='$idEntreprise'";
-	$result = Connexion::table($requeteEntrepriseFournisseur);
-    return $result;
-}
-
-function bonDeLivraisonEntrepriseCliente($idClient)
-{
-	$requeteEntrepriseCliente="SELECT organisation.codePostal,organisation.adresse,organisation.ville,organisation.numero,organisation.libelle 
-								FROM client,organisation 
-								WHERE client.entreprise_id=organisation.id 
-								AND client.id='$idClient'";
-	$result = Connexion::table($requeteEntrepriseCliente);
-    return $result;
-}
-
-function bonDeLivraisonProduit($idDevis)
-{
-	$requeteProduit="SELECT produit.libelle,produit.reference,ligneDevis.quantite
-					FROM devis,ligneDevis,produit
-					WHERE produit.id=ligneDevis.produit_id
-					AND ligneDevis.devis_id=devis.id
-					AND devis.id='$idDevis'";
-	$result = Connexion::table($requeteProduit);
-	return $result;
-}
 ?>
--- mod_glt/fonctionsBonDeLivraison.php
+++ mod_glt/fonctionsBonDeLivraison.php
@@ -9,7 +9,21 @@ function bonDeLivraisonEntrepriseFournisseur($idEntreprise)
 
 function bonDeLivraisonEntrepriseCliente($idClient)
 {
-	$requeteEntrepriseCliente="SELECT nom,codePostal,adresse,ville,tel FROM fournisseur WHERE id='$idClient'";
+	$requeteEntrepriseCliente="SELECT organisation.codePostal,organisation.adresse,organisation.ville,organisation.numero,organisation.libelle 
+								FROM client,organisation 
+								WHERE client.entreprise_id=organisation.id 
+								AND client.id='$idClient'";
 	$result = Connexion::table($requeteEntrepriseCliente);
     return $result;
+}
+
+function bonDeLivraisonProduit($idDevis)
+{
+	$requeteProduit="SELECT produit.libelle,produit.reference,ligneDevis.quantite,produit.montant
+					FROM devis,ligneDevis,produit
+					WHERE produit.id=ligneDevis.produit_id
+					AND ligneDevis.devis_id=devis.id
+					AND devis.id='$idDevis'";
+	$result = Connexion::table($requeteProduit);
+	return $result;
 }
