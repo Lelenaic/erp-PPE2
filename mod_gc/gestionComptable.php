84413cbaa00f252d733956fd1eb027bb70d0bb64
fzf <fesfz@gmail.com>
2016-03-15 08:20:40 +0100

    Mise à jour

--- mod_gc/gestionComptable.php
+++ mod_gc/gestionComptable.php
@@ -4,6 +4,9 @@ function formDevis_route(){
     $form=new FormBootstrap();
     $form->addHidden('route','gc_gestionComptable_ajouter');
     $list=array();
+    $list1=array();
+    $list2=array();
+    $list3=array();
     $employe=  Connexion::table('select * from employe');
     foreach ($employe as $ut){
         $list0[$ut['id']]=$ut['nom'];
@@ -19,30 +22,22 @@ function formDevis_route(){
     foreach ($produit as $ut){
         $list2[$ut['id']]=$ut['libelle'];
     }
-    $form->addSelect('produit_id', $list2, array(), 'Nom du produit');
-    $form->addText('quantite', array(), 'Quantité');
-    $form->addText('prix', array(), 'Prix');
     
     // Insert le fichier de gestion des formulaires défini dans le modèle Boostrap
     include(ROOT.'AdminLTE/form.php');    
 }
+$form->addSelect('produit_id', $list2, array(), 'Nom du produit');
+    $validation=  Connexion::table('select * from validation');
+    foreach ($validation as $ut){
+        $list3[$ut['id']]=$ut['libelle'];
+    }
+    $form->addSelect('validation_id', $list3, array(), 'Validation');
+    $form->addText('quantite', array(), 'Quantité');
+    $form->addText('prix', array(), 'Prix');
 //ajouter route
-/*function ajouter_route(){
-    $employe=$_POST['employe_id'];
-    $client=$_POST['client_id'];
-    $date=$_POST['date'];
-    $nomProduit=$_POST['produit_id'];
-    $quantite=$_POST['quantite'];
-    $prix=$_POST['prix']; 
-    $query1='INSERT INTO devis (employe_id, client_id, date, prix)'
-            . "VALUES ('".$employe."', '".$client."', '".$date."', '".$prix."')";
-    Connexion::exec($query1);
-    $query2='INSERT INTO ligneDevis (produit_id, quantite)'
-            . "VALUES ('".$nomproduit."', '".$quantite."')";
-    Connexion::exec($query2);
-    include(ROOT.'AdminLTE/alerte.php');
+function ajouter_route(){
+    include(ROOT.'mod_gc/alerteDevis.php');
 }
-*/
 function listeFacture_route(){
     $utilisateurs=Connexion::table('select login,utilisateurtype_id from utilisateur order by login');
     include(ROOT.'mod_gc/listeFacture.php');
